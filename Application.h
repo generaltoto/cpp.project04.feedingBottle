c2450c82fa8e9e46799af67cdf696b0263bda111
Thomas <tdelorme@gaming.tech>
2022-11-09 15:42:38 +0100

    Added bottle management functions

--- Application.h
+++ Application.h
@@ -11,11 +11,21 @@ class Application
 {
 private:
 	std::list<FeedingBottle> bottleList;
+	bool isAutomaticCommand = false;
 
 	SDL_Window* window = NULL;
 	SDL_Surface* screenSurface = NULL;
 
 public:
+
+	/** Adds a bottle to the [bottleList] */
+	void addBottle(BottleCommandTemplate command, int bottleCapacity = 330);
+
+	/** Sets a timer with the bottle delivery date depending on the current date */
+	void setTimer(FeedingBottle bottle);
+
+	/** Launches a command or an automatic one if selected. */
+	void launchCommand(void);
 
 	/** Returns the window used by SDL */
 	SDL_Window* getWindow(void);
